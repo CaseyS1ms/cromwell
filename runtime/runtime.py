eabab2cbf565e5ef214dd5b612a2f8b086753b87
CaseySims <caseysims07@gmail.com>
2026-08-18 20:20:03 +0100

    basic loop setyp

--- runtime/runtime.py
+++ runtime/runtime.py
@@ -4,7 +4,7 @@ simulator = Simulator()
 
 simulator.setup(1)
 
-for i in range(50):
+for i in range(500000):
     if len(simulator.pop_list) == 0:
         print("No Agents Left - Simulation Complete at tick", + simulator.tick)
         break
--- runtime/simulator.py
+++ runtime/simulator.py
@@ -6,19 +6,32 @@ from agents.agent import Agent
 class Simulator:
 
     def __init__(self):
-        self.agent = Agent()
-        self.pop_list = []
         self.tick = 0
+        self.pop_list = []
+
 
     def setup(self, num_agents):
-        self.pop_list = np.array([Agent() for _ in range(num_agents)])
+        self.pop_list = np.array([Agent(self) for _ in range(num_agents)])
 
     def step(self):
         for agent in self.pop_list:
             agent.step()
 
-        self.tempPopList = [agent for agent in self.pop_list if agent.alive]
-        self.pop_list = self.tempPopList
+        if self.tick == 120:
+            self.pop_list.append(Agent(self))
+
+        self.pop_list = [agent for agent in self.pop_list if agent.alive]
+
+
+        if self.tick % 8760 == 0:
+            print("Year", self.tick // 8760)
+
+        if self.tick % 730 == 0:
+            print("Month", self.tick // 730)
+
+
+
+        # self.pop_list = self.tempPopList
 
 
 
