0034db6baa73dd0ebbd610d5b6321f567b8bf34e
CaseySims <caseysims07@gmail.com>
2026-08-18 20:20:03 +0100

    basic loop setup

--- agents/agent.py
+++ agents/agent.py
@@ -1,22 +1,26 @@
 class Agent:
 
-    def __init__(self):
+    def __init__(self,simulator):
         self.health = 100
-        self.hunger = 100
+        self.hunger = 0
         self.alive = True
-
+        self.tick = simulator.tick
+        # print("Agent Created at tick ", self.tick)
 
     def step(self):
 
-        self.hunger = min(100, self.hunger + 1)
+        self.hunger = min(100, self.hunger + 4.16)
+        # print("Hunger: ", self.hunger, " Health: ", self.health)
 
-        if self.hunger > 80:
-            self.health -= 1
+        if self.hunger == 100:
+            self.health = max(0, self.health - 1)
 
         if self.health <= 0:
-            print("Agent Died")
+            # print("Agent Died at tick ", self.tick)
             self.alive = False
             return
+
+        self.tick += 1
 
         #REST OF STEP TO HAPPEN EACH TICK
 
